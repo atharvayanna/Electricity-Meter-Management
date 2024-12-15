ace04030dae39ca27e6caa0ccf067e148bafc623
atharvayanna <atharva.yanna@emergys.com>
2024-12-15 15:32:54 +0530

    login integrated

--- Electricity Meter Management/src/redux/store.js
+++ Electricity Meter Management/src/redux/store.js
@@ -4,22 +4,19 @@ import loginSlice from './slices/loginSlice';
 import adminSlice from './slices/admin/adminSlice';
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import loginSlice
 
-// Persist config for appSlice
 const persistConfig = {
     key: "root",
     storage,
 };
 
-// Apply persist only to the appSlice (since you're using persistedReducer for appSlice)
 const persistedAppReducer = persistReducer(persistConfig, appSlice);
 
 export const store = configureStore({
     reducer: {
-        app: persistedAppReducer, // Persisted app slice
-        login: loginSlice,         // Regular login slice
-        admin: adminSlice,         // Regular admin slice
+        app: persistedAppReducer, 
+        login: loginSlice,         
+        admin: adminSlice,         
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
